83067160e5506ca1915dedec122141e7549f564c
Aidanatoi <toichubekova_a@auca.kg>
2022-06-06 17:35:19 +0600

    03-01/added

---
+++ script.js
@@ -0,0 +1,49 @@
+const elementa=10
+if (elementa== 10) {
+	alert('Верно');
+} else {
+	alert('Неверно');
+}
+
+const numa = 1;
+if (numa>0) {
+                    alert('Right!');
+                    } else {
+                    alert('Wrong...');
+                    };
+
+
+
+
+const str= 'test '
+const strtr= true
+if (confirm ('test')) {
+ console.log ('true')
+} else {
+ console.log ('false')
+}
+
+const vara = 2;
+const varb = 15;
+if (vara > 2 && vara < 11 || varb >= 6 && varb < 14) {
+alert('Правильно');
+} else {
+alert('Неверно');
+};
+
+const a = 1;
+let result;
+switch (a) {
+case 1:
+alert(result = 'Winter');
+break;
+case 2:
+alert(result = 'Spring');
+break;
+case 3:
+alert(result = 'Summer');
+break;
+case 4:
+alert(result = 'Fall');
+break;
+};
